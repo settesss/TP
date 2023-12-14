6afaf0841c65038fad7e72cc1d5e10088440e4bc
Nikita <nnmanakovv@gmail.com>
2023-12-15 02:47:00 +0700

    add: добавлены стресс-тесты

---
+++ Testing/TablePlugin.StressTesting/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace TablePlugin.StressTesting
+{
+    using System.Diagnostics;
+    using System.IO;
+    using System;
+    using Microsoft.VisualBasic.Devices;
+    using TablePlugin.Connector;
+    using TablePlugin.Model;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new TableBuilder();
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var parameters = new Parameters();
+            parameters.ParamsDictionary = new Dictionary<ParameterType.ParamType, Parameter>()
+            {
+                { ParameterType.ParamType.TableLength, new Parameter(1200, 600, 1200) },
+                { ParameterType.ParamType.TableWidth, new Parameter(1200, 600, 1200) },
+                { ParameterType.ParamType.TableHeight, new Parameter(500, 400, 500) },
+                { ParameterType.ParamType.ShelfLength, new Parameter(1100, 300, 1100) },
+                { ParameterType.ParamType.ShelfWidth, new Parameter(1100, 300, 1100) },
+                { ParameterType.ParamType.ShelfHeight, new Parameter(40, 10, 40) },
+                { ParameterType.ParamType.SupportSize, new Parameter(50, 30, 50) },
+                { ParameterType.ParamType.ShelfFloorDistance, new Parameter(360, 30, 360) },
+                { ParameterType.ParamType.BracingSize, new Parameter(45, 20, 45) },
+                { ParameterType.ParamType.WheelSize, new Parameter(70, 0, 70) },
+            };
+            var streamWriter = new StreamWriter($"log.txt", true);
+            Process currentProcess = System.Diagnostics.Process.GetCurrentProcess();
+            var count = 0;
+
+            while (count < 71)
+            {
+                const double gigabyteInByte = 0.000000000931322574615478515625;
+                builder.Build(parameters);
+                var computerInfo = new ComputerInfo();
+                var usedMemory = (computerInfo.TotalPhysicalMemory - computerInfo.AvailablePhysicalMemory) * gigabyteInByte;
+                streamWriter.WriteLine($"{++count}\t{stopWatch.Elapsed:hh\\:mm\\:ss}\t{usedMemory}");
+                streamWriter.Flush();
+            }
+
+            stopWatch.Stop();
+            streamWriter.Close();
+            streamWriter.Dispose();
+            Console.Write($"End {new ComputerInfo().TotalPhysicalMemory}");
+        }
+    }
+}
